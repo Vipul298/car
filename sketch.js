bdca45ef330a2699104c44bca4b2c7317bf72b90
Vipul298 <75067122+Vipul298@users.noreply.github.com>
2021-03-30 15:28:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var gameState = 0;
+var playerCount;
+var database;
+var form,player,game;
+var allPlayers;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    background("white");
+
+
+}
